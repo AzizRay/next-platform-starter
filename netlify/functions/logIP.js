c5b91144c89a0e77ecba8a8f7720081c5f495727
AzizRay <itsazizray@gmail.com>
2025-02-19 19:23:34 -0500

    Updated function to log IP but return 404

--- netlify/functions/logIP.js
+++ netlify/functions/logIP.js
@@ -1,11 +1,12 @@
 exports.handler = async function (event, context) {
-    const ip = event.headers["x-forwarded-for"] || event.headers["client-ip"] || "Unknown IP";
-    console.log(`Incoming request from IP: ${ip}`);
-  
-    return {
-      statusCode: 404,
-      headers: { "Content-Type": "text/plain" },
-      body: "404 Not Found",
-    };
+  const ip = event.headers["x-forwarded-for"] || event.headers["client-ip"] || "Unknown IP";
+
+  // Log the IP address to Netlify's function logs
+  console.log(`Incoming request from IP: ${ip}`);
+
+  return {
+    statusCode: 404,
+    headers: { "Content-Type": "text/plain" },
+    body: "404 Not Found",
   };
-  
+};
